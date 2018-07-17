6357c1209b2e4b8755755c5da7deeafab7a7b00d
Rinalds Pudulis <rpudulis@gmail.com>
2018-07-17 14:41:01 +0300

    feat: Creating navigation components

---
+++ src/components/SignOut.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignOutPage = () => {
+    return (
+        <div>
+            <h1>Sign Out page</h1>
+        </div>
+    );
+};
+
+export default SignOutPage;
